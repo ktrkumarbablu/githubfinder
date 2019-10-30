8f46d3430ae336aff36656493a4f5786f9b97412
ktrkumarbablu <ktrkumarbablu@gmail.com>
2019-10-30 18:01:51 +0530

    more style sheet add

--- src/component/users/UserItem.js
+++ src/component/users/UserItem.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react'
 export class UserItem extends Component {
 
     render() {
-        const {avatar_url}=this.props.user;
+        const {avatar_url,login,html_url}=this.props.user;
         return (
             <div className="card text-center">
                 <img src={avatar_url} alt=""
                      className="round-img" style={{width:'60px'}}
                 ></img>
+             <h3>{login}</h3>
+             <div>
+                 <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+             </div>
             </div>
         )
     }
